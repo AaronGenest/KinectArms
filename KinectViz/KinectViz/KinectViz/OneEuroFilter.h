a6d6ce9b71160bfeeb9db1fa68ef2340b37e99b9
Zenja Ivkovic <izenja@gmail.com>
2012-11-06 11:49:23 -0600

    Comment explaining one euro filter usage

--- KinectViz/KinectViz/KinectViz/OneEuroFilter.h
+++ KinectViz/KinectViz/KinectViz/OneEuroFilter.h
@@ -1,5 +1,6 @@
 #pragma once
 
+// Support class for the OneEuroFilter
 class LowPassFilter {
 public:
 	LowPassFilter() : firstTime(true) { }
@@ -15,14 +16,24 @@ private:
 };
 
 
-// One Euro filter, based on algorithm from paper by Gery Casiez, Nicolas Roussel, and Daniel Vogel (CHI 2012)
+/*
+ * One Euro filter, based on algorithm from paper by Gery Casiez, Nicolas Roussel, and Daniel Vogel (CHI 2012).
+ * Three parameters must be set and tuned based on your application:
+ * - minCutoffFreq: minimum cutoff frequency in Hz
+ * - derivCutoffFreq: minimum cutoff frequency in Hz for the value derivative
+ * - cutoffSlope: the slope of the cutoff line (also refered to as beta in the paper)
+ * Reasonable starting parameters are 1.0 for minCutoffFreq and derivCutoffFreq, and 0.1 for cutoffSlope
+ * if your values range up to around 100-1000. If your values are lower than this, decrease cutoffSlope.
+ */
 class OneEuroFilter {
 public:
-	OneEuroFilter(float minCutoff, float minDerivCutoff, float cutoffSlope);
+	OneEuroFilter(float minCutoffFreq, float derivCutoffFreq, float cutoffSlope);
 	~OneEuroFilter() { }
 
+	// Filters a new data point (x) and returns the filtered value. Expects timeElapsed to be time in milliseconds since last call to filter().
 	float filter(float x, float timeElapsed);
 
+	// Tweakable parameters
 	float minCutoffFreq;
 	float derivCutoffFreq;
 	float cutoffSlope;
